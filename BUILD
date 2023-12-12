d8493273b9298748229d7707b6b5d65d613f325b
Blake Caldwell <blake@blakes-mbp-2.lan>
2023-12-12 01:28:21 -0500

    cleanup part 2

--- BUILD
+++ BUILD
@@ -22,10 +22,7 @@ cc_binary(
       "day2.cc"
       ],
     deps = [
-      "@com_google_absl//absl/flags:flag",
       "@com_google_absl//absl/flags:parse",
-      "@com_google_absl//absl/strings",
       "@com_google_absl//absl/status:statusor",
-      "@com_google_absl//absl/container:flat_hash_set",
     ]
 )
--- day2.cc
+++ day2.cc
@@ -1,110 +1,33 @@
-#include <iostream>
 #include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
-#include "absl/strings/str_cat.h"
 #include "absl/status/statusor.h"
-#include "absl/container/flat_hash_set.h"
-#include "absl/container/flat_hash_map.h"
-#include "absl/strings/numbers.h" 
 #include <fstream>
-#include <string>
-#include <iostream>
-#include <sstream>
-#include <cstdlib>
-#include <memory>
 
 ABSL_FLAG(std::string, input_file, "", "Input file to process");
 
-void ProcessFilePart1(const std::string& filename);
-void ProcessFilePart2(const std::string& filename);
+void ProcessFile(const std::string& filename);
 
 int main(int argc, char* argv[]) {
   absl::ParseCommandLine(argc, argv);
   std::string file_path = absl::GetFlag(FLAGS_input_file);
-  ProcessFilePart2(file_path);
+  ProcessFile(file_path);
 }
 
-void ProcessFilePart1(const std::string& filename) {
+void ProcessFile(const std::string& filename) {
   std::ifstream file(filename);
   std::string line;
+  int line_num = 0;
+  int part_1_sum = 0;
+  int part_2_sum = 0;
 
-  int sum = 0;
-  while (std::getline(file, line)) {
-    // Extract the game ID
-    size_t start = line.find("Game ") + 5; // Start after "Game "
-    size_t end = line.find(':', start); // Find the colon after "Game "
-    if (start == std::string::npos || end == std::string::npos) {
-      continue; // No "Game " or colon found, skip the line
-    }
-
-    std::string game_id_str = line.substr(start, end - start);
-    std::string game_data = line.substr(end + 1);
-    std::stringstream roll_ss(game_data);
-    std::string roll_item, color_item;
-
-    int32_t game_id;
-    if (!absl::SimpleAtoi(game_id_str, &game_id)) {
-      continue; // If conversion fails, skip the line
-    }
-
-    bool possible = true;
-    int red_count = 0;
-    int green_count = 0;
-    int blue_count = 0;
-
-    // each roll
-    while (possible && std::getline(roll_ss, roll_item, ';')) {
-      std::cout << "Parsing roll:" << roll_item << "\n";
-
-      // each color
-      std::stringstream color_ss(roll_item);
-      while (possible && std::getline(color_ss, color_item, ',')) {
-        std::stringstream item_stream(color_item);
-        int32_t count;
-        std::string color;
-
-        if (item_stream >> count >> color) {
-          if (color == "red") {
-            if (count > red_count) {
-              red_count = count;
-            }
-          } else if (color == "green") {
-            if (count > green_count) {
-              green_count = count;
-            }
-          } else if (color == "blue") {
-            if (count > blue_count) {
-              blue_count = count;
-            }
-          }
-        }
-      } // each color
-      if (red_count > 12 || green_count > 13 || blue_count > 14) {
-        possible = false;
-      }
-    } // each roll
-
-    if (possible) {
-      sum += game_id;
-    }
-  
-  } // each game
-
-  std::cout << "Total sum: " << sum << "\n";
-}
-
-void ProcessFilePart2(const std::string& filename) {
-  std::ifstream file(filename);
-  std::string line;
-
-  int sum = 0;
-  
   // each game
   while (std::getline(file, line)) {
+    line_num++;
     // Extract the game ID
     size_t start = line.find("Game ") + 5; // Start after "Game "
     size_t end = line.find(':', start); // Find the colon after "Game "
     if (start == std::string::npos || end == std::string::npos) {
+      std::cout << "Couldn't find 'Game ' on line " << line_num << ", skipping: " << line << "\n";
       continue; // No "Game " or colon found, skip the line
     }
 
@@ -115,9 +38,11 @@ void ProcessFilePart2(const std::string& filename) {
 
     int32_t game_id;
     if (!absl::SimpleAtoi(game_id_str, &game_id)) {
+      std::cout << "Couldn't parse game_id on line " << line_num << ", skipping: " << line << "\n";
       continue; // If conversion fails, skip the line
     }
 
+    bool part_1_possible = true;
     int red_count = 0;
     int green_count = 0;
     int blue_count = 0;
@@ -132,7 +57,6 @@ void ProcessFilePart2(const std::string& filename) {
         std::stringstream item_stream(color_item);
         int32_t count;
         std::string color;
-
         if (item_stream >> count >> color) {
           if (color == "red") {
             if (count > red_count) {
@@ -149,12 +73,19 @@ void ProcessFilePart2(const std::string& filename) {
           }
         }
       } // each color
-      
+
+      if (red_count > 12 || green_count > 13 || blue_count > 14) {
+        part_1_possible = false;
+      }
     } // each roll
 
-    sum += green_count * blue_count * red_count;
-    
+    if (part_1_possible) {
+      part_1_sum += game_id;
+    }
+    part_2_sum += green_count * blue_count * red_count;
   } // each game
 
-  std::cout << "Total sum: " << sum << "\n";
+  std::cout << std::endl;
+  std::cout << "Part 1 sum: " << part_1_sum << "\n";
+  std::cout << "Part 2 sum: " << part_2_sum << "\n";
 }
