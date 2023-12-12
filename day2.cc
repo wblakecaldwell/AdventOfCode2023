d1d849a91d86be3d503c3087defbab0ca6074e7e
Blake Caldwell <blake@blakes-mbp-2.lan>
2023-12-12 01:16:09 -0500

    rough draft day 2, part 2

--- day2.cc
+++ day2.cc
@@ -15,15 +15,16 @@
 
 ABSL_FLAG(std::string, input_file, "", "Input file to process");
 
-void ProcessFile(const std::string& filename);
+void ProcessFilePart1(const std::string& filename);
+void ProcessFilePart2(const std::string& filename);
 
 int main(int argc, char* argv[]) {
   absl::ParseCommandLine(argc, argv);
   std::string file_path = absl::GetFlag(FLAGS_input_file);
-  ProcessFile(file_path);
+  ProcessFilePart2(file_path);
 }
 
-void ProcessFile(const std::string& filename) {
+void ProcessFilePart1(const std::string& filename) {
   std::ifstream file(filename);
   std::string line;
 
@@ -87,6 +88,72 @@ void ProcessFile(const std::string& filename) {
       sum += game_id;
     }
   
+  } // each game
+
+  std::cout << "Total sum: " << sum << "\n";
+}
+
+void ProcessFilePart2(const std::string& filename) {
+  std::ifstream file(filename);
+  std::string line;
+
+  int sum = 0;
+  
+  // each game
+  while (std::getline(file, line)) {
+    // Extract the game ID
+    size_t start = line.find("Game ") + 5; // Start after "Game "
+    size_t end = line.find(':', start); // Find the colon after "Game "
+    if (start == std::string::npos || end == std::string::npos) {
+      continue; // No "Game " or colon found, skip the line
+    }
+
+    std::string game_id_str = line.substr(start, end - start);
+    std::string game_data = line.substr(end + 1);
+    std::stringstream roll_ss(game_data);
+    std::string roll_item, color_item;
+
+    int32_t game_id;
+    if (!absl::SimpleAtoi(game_id_str, &game_id)) {
+      continue; // If conversion fails, skip the line
+    }
+
+    int red_count = 0;
+    int green_count = 0;
+    int blue_count = 0;
+
+    // each roll
+    while (std::getline(roll_ss, roll_item, ';')) {
+      std::cout << "Parsing roll:" << roll_item << "\n";
+      std::stringstream color_ss(roll_item);
+
+      // each color
+      while (std::getline(color_ss, color_item, ',')) {
+        std::stringstream item_stream(color_item);
+        int32_t count;
+        std::string color;
+
+        if (item_stream >> count >> color) {
+          if (color == "red") {
+            if (count > red_count) {
+              red_count = count;
+            }
+          } else if (color == "green") {
+            if (count > green_count) {
+              green_count = count;
+            }
+          } else if (color == "blue") {
+            if (count > blue_count) {
+              blue_count = count;
+            }
+          }
+        }
+      } // each color
+      
+    } // each roll
+
+    sum += green_count * blue_count * red_count;
+    
   } // each game
 
   std::cout << "Total sum: " << sum << "\n";
